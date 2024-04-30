4c630487fd6996bd61395fb64aa921ed54e24fcf
PierreAlura <168541768+PierreAlura@users.noreply.github.com>
2024-04-30 08:55:14 -0300

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,1 +1,19 @@
+function setup(){
+  createCanvas(400,400);
+}
+
+function draw(){
+  background("white");
+
+  stroke("blue");
+  fill("red");
+
+  if(mousepressed){
+
+    rect(mouseX, mouseX, 20, 35)
+  }
+
+  console.log(mouseIsPressed);
+  rect(mouseX, mouseX, 100, 150)
+}
 
